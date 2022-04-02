714d4d587ada2aa08c7f2dac4847c2a696939984
eduardoddmg <eduardoddmg@gmail.com>
2022-04-01 21:27:32 -0300

    update

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -6,7 +6,7 @@ import { Button } from "react-bootstrap";
 
 const Principal = () => {
   return (
-    <div style={{height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{height: '40vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '0 3vh'}}>
         <h3>O melhor lugar para você comer é aqui</h3>
       <Link to="/cardapio" style={{width: '30%', textAlign: 'center'}}>
         <Button variant='primary'>Cardápio</Button>
